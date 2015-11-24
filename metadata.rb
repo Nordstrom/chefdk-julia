39d7b5dac1b55cac26761879592bdeb3c54abcf3
Orion Ifland <orion.ifland@nordstrom.com>
2015-11-24 13:00:29 -0800

    Update the metadata to match the cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,7 @@
-name             'code_generator'
-description      'Custom code generator cookbook for use with ChefDK'
-long_description 'Custom code generator cookbook for use with ChefDK'
+# ChefDK requires this metadata file to be able to load Julia as a cookbook, even
+# though Julia isn't a traditional cookbook that gets uploaded to Chef Server
+# and run by nodes.
+name             'chefdk-julia'
+description      'An Opinionated Cookbook Creator'
+long_description 'An Opinionated Cookbook Creator, with spunk and a grin, by Nordstrom'
 version          '0.1.0'
-
